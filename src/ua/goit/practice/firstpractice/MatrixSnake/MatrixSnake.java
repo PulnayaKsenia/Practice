4036f029ae9d58c4f90fae0b6ddb24a1ac633d5b
PulnayaKsenia <pulnayaksenia@gmail.com>
2016-09-15 22:58:07 +0300

    universal matrix

--- src/ua/goit/practice/firstpractice/MatrixSnake/MatrixSnake.java
+++ src/ua/goit/practice/firstpractice/MatrixSnake/MatrixSnake.java
@@ -4,24 +4,24 @@ import java.util.Arrays;
 
 public class MatrixSnake {
     public static void main(String[] args) {
-        int[][] matrix = {{1, 8, 9, 16},
-                {2, 7, 10, 15},
-                {3, 6, 11, 14},
-                {4, 5, 12, 13}};
+        int[][] matrix = {{1, 6, 7},
+                {2, 5, 8},
+                {3, 4, 9}};
 
-        int [] matrixSnake = new int[matrix.length * matrix.length];
+        int[] matrixSnake = new int[matrix.length * matrix.length];
         int index = 0;
 
         for (int y = 0; y < matrix.length; y++) {
-            for (int x = 0; x < matrix.length; x++) {
-                matrixSnake[index] = matrix[x][y];
-                index++;
-            }
-            y++;
-
-            for (int x = matrix.length - 1; x >=0; x--) {
-                matrixSnake[index] = matrix[x][y];
-                index++;
+            if (y % 2 == 0) {
+                for (int x = 0; x < matrix.length; x++) {
+                    matrixSnake[index] = matrix[x][y];
+                    index++;
+                }
+            } else {
+                for (int x = matrix.length - 1; x >= 0; x--) {
+                    matrixSnake[index] = matrix[x][y];
+                    index++;
+                }
             }
         }
         System.out.println(Arrays.toString(matrixSnake));
