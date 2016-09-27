d71e65bd7ebca8ebc9023f5c7461c1dca8522780
PulnayaKsenia <pulnayaksenia@gmail.com>
2016-09-27 23:58:42 +0300

    for 2.1 module

--- src/ua/goit/practice/firstpractice/Base36/Base36.java
+++ src/ua/goit/practice/firstpractice/Base36/Base36.java
@@ -1,8 +1,9 @@
 package ua.goit.practice.firstpractice.Base36;
 
 public class Base36 {
+    private final static String symbolsForBase36 = "0123456789abcdefghijklmnopqrstuvwxyz";
+
     public static String summingBase36(String symbols) {
-        String symbolsForBase36 = "0123456789abcdefghijklmnopqrstuvwxyz";
         int numberOfSymbols = 36;
         String[] split = symbols.split(",");
         long sumOfNumber = 0;
@@ -10,9 +11,13 @@ public class Base36 {
 
         for (String symbol : split) {
             for (char symbolChar : symbol.toCharArray()) {
-                count += symbolsForBase36.indexOf(symbolChar);
-                System.out.println(symbolsForBase36.indexOf(symbolChar));
-                count += numberOfSymbols;
+                for (int i = 0; i < numberOfSymbols; i++) {
+                    if (symbolChar == symbolsForBase36.charAt(i)) {
+                        count +=i;
+                        break;
+                    }
+                }
+                count *= numberOfSymbols;
             }
             sumOfNumber += count/numberOfSymbols;
             count = 0;
@@ -21,8 +26,8 @@ public class Base36 {
     }
 
     public static void main(String[] args) {
-        String symbolsForSum = "a12, 89z";
-        System.out.println("Result after summing '" + symbolsForSum + "': " + summingBase36(symbolsForSum));
+        String symbolsForSum = "z, 1";
+        System.out.println("Result after summing " + symbolsForSum + ": " + summingBase36(symbolsForSum));
     }
 }
 
